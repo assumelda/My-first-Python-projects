acf53dec586b38240485cda012d138fafc915a2e
assumelda <assumelda@gmail.com>
2025-11-24 13:52:42 +0100

    Uncomment codes

--- Band generator.py
+++ Band generator.py
@@ -1,22 +1,22 @@
-# #repeating the smae line of code multiple times in differnt lines
-# # print("Hello world!\nHello world!\nHello world!")
-# # print("Hello " + "Angela")
-# #Colleting data from the user
-# # print("Hello "+ input("What is your Name?") + "!")
-# #Variable decleration
-# name="Jack"
-# print(name)
+#repeating the smae line of code multiple times in differnt lines
+print("Hello world!\nHello world!\nHello world!")
+print("Hello " + "Angela")
+#Colleting data from the user
+print("Hello "+ input("What is your Name?") + "!")
+#Variable decleration
+name="Jack"
+print(name)
 
-# name="Chisimdi"
-# print(name)
+name="Chisimdi"
+print(name)
 
-# # # # name=input("What is your name?")
-# # # # print("My name is " +name)
-# # # # print(len(name))
-# # # print(len(input("What is your name?")))
-# Username=input("What is your name?")
-# Length=len(Username)
-# print(Length)
+name=input("What is your name?")
+print("My name is " +name)
+print(len(name))
+print(len(input("What is your name?")))
+Username=input("What is your name?")
+Length=len(Username)
+print(Length)
 
 print("Welcome to the Band Name Generator.")
 City_name=input("what is the name of the City you grew up in?\n")
